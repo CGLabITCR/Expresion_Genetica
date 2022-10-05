2901594b4661d55f27eb13d3ff0e7f1162b339ee
Rogelio <rojo@tec.ac.cr>
2022-10-05 15:56:29 -0600

    Mensajes On Screen

--- Assets/Code/Scripts/Obj_Controller.cs
+++ Assets/Code/Scripts/Obj_Controller.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+
 
 public class Obj_Controller : MonoBehaviour
 {
     Material original_trigger_material;
+    public Text notificationText;
+
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +30,9 @@ public class Obj_Controller : MonoBehaviour
         if (other.gameObject.CompareTag(this.gameObject.tag))
         {
             original_trigger_material.color =  new Color(0f, 1f, 0f, 0.5f);
+
+            StartCoroutine(sendNotification("hola", 3));
+            
         }
         else {
             original_trigger_material.color = new Color(1f, 0f, 0f, 0.5f);
@@ -35,5 +43,14 @@ public class Obj_Controller : MonoBehaviour
     private void OnTriggerExit(Collider other)
     {
         original_trigger_material.color = new Color(0f, 0f, 1f, 0.5f);
+    }
+
+
+    IEnumerator sendNotification(string text, int time)
+    {
+        notificationText.text = text;
+        yield return new WaitForSeconds(time);
+        notificationText.text = " ";
+
     }
 }
